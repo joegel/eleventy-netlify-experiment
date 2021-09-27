ea0304ff306b20cd36371917a412891c4abc2420
Joe <jig813@gmail.com>
2021-09-27 10:16:58 +0300

    troubleshoot 2.0

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,4 @@
 module.exports = function(eleventyConfig) {
-    eleventyConfig.setTemplateFormats([
-        "md",
-        "css",
-        "svg",
-        "png",
-        "jpg"
-    ]);
     eleventyConfig.addPassthroughCopy("images");
-    eleventyConfig.addPassthroughCopy("admin");
+    eleventyConfig.addPassthroughCopy("admin")
 };
